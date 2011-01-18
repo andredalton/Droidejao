7baa5b059ac9ad7459e94bfdcb81ee9c020f4946
André Meneghelli <andredalton@gmail.com>
2011-01-18 21:54:51 -0200

    Droidejao versao Santa Ceia 0.1

--- src/br/usp/ime/droidejao/Droidejao.java
+++ src/br/usp/ime/droidejao/Droidejao.java
@@ -2,7 +2,6 @@ package br.usp.ime.droidejao;
 
 import android.app.Activity;
 import android.os.Bundle;
-import java.net;
 
 public class Droidejao extends Activity
 {
@@ -11,6 +10,6 @@ public class Droidejao extends Activity
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.capa);
     }
 }
