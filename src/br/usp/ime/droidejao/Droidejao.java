0af1106efc541c173a52f1f7e6133226fd92d135
André Meneghelli <andredalton@gmail.com>
2011-01-19 01:52:40 -0200

    sem alterar orientacao da tela

--- src/br/usp/ime/droidejao/Droidejao.java
+++ src/br/usp/ime/droidejao/Droidejao.java
@@ -1,15 +1,79 @@
+/*
+ * Copyright (C) 2007 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package br.usp.ime.droidejao;
+
+// import com.example.android.apis.R;
 
 import android.app.Activity;
 import android.os.Bundle;
 
+/**
+ * <p>This example shows how to use cell spanning in a table layout.</p>
+ */
+public class Droidejao extends Activity {
+//    private boolean mShrink;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
+
+
+
+
+
+
+/*
+
+
+
+
+
+
+package br.usp.ime.droidejao;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
+
+
 public class Droidejao extends Activity
 {
-    /** Called when the activity is first created. */
+    /** Called when the activity is first created. *
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.capa);
+        setContentView(R.layout.main);
     }
 }
+
+*/
